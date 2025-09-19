ac9355e55a47bbc794b3ca308cc656e8d32bcbd8
JayEsEichi <96898059+JayEsEichi@users.noreply.github.com>
2025-09-19 15:06:16 +0900

    next image 빌드 옵션 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  images: {
+    // 빌드/런타임에서 sharp 안 쓰기
+    unoptimized: true,
+    // PNG/JPG 등을 `import`로 집어넣지 않게 함 (원인은 import trace의 png)
+    disableStaticImages: true,
+  },
 };
 
 export default nextConfig;
